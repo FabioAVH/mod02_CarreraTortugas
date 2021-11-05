9d776e454e68269d32c98983df3bc49be00662bd
unknown <you@example.comfabio.vargas@opexa.co>
2021-11-04 22:55:08 -0500

    Actualizacion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 class Circuito():
     corredores = []
@@ -15,9 +16,10 @@ class Circuito():
         self.__screen.bgcolor('lightgray')
         self.__startLine = -width/2+20
         self.__finishLine = width/2-20
+                
+        self.__createRunner() #esto crea los corredores cuando se crea la clase
         
-        self.__createRunner()
-        
+    
     def __createRunner(self):
         
         for i in range(4):
@@ -29,12 +31,18 @@ class Circuito():
             
             self.corredores.append(new_turtle)
             
-
-
-
-
-
-
+    
+    def Correr(self):
+        
+        hayGanador = False
+        
+        while not hayGanador:
+            for tortuga in self.corredores:
+                tortuga.forward(random.randint(1,6))
+                if tortuga.position()[0] >= self.__finishLine:
+                    hayGanador = True
+                    print('La tortuga {} ha ganado!!!'.format(tortuga.color()[0]))    
 
 if __name__ == '__main__':
     circuito = Circuito(640,480)
+    circuito.Correr()
